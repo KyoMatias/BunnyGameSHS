96c9f6767464e3c0e4f5e956d6852b8396de7fd0
Kyoko <108702599+KyoMatias@users.noreply.github.com>
2025-01-26 08:10:05 +0800

    -Added GameManager, And fixed some scene parameters on Prologue.Unity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class GameManager : MonoBehaviour
 {
+    // Singleton Instance of the GameManager.
+    public static GameManager Instance { get; private set;}
+
+    private void Awake() {
+        InitializateComponents();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,17 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void InitializateComponents()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        } 
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 }
